6df23341278e6df9756fd7d2faa1fba5707714ae
ahmedmontaser98 <62159198+ahmedmontaser98@users.noreply.github.com>
2023-04-12 15:41:24 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const bodyParser = require('body-parser');
+const {v4:uuidv4} = require('uuid');
+const app = express();
+
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, uuidv4());
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype === 'image/jpg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+app.use(bodyParser.json()); //application/json
+
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode;
+    const message = error.message;
+    res.status(status).json({message: message});
+});
+mongoose.connect('')
+.then(result => {
+    const server = app.listen(8080);
+    const io = require('./socket').init(server);
+    io.on('connection', socket => {
+        console.log('Client connected!');
+    })
+})
+.catch(err => console.log(err));
